08d7931bb5ce49214b2933837aaa694dc661b58a
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-11-03 14:57:08 -0300

    Configuracion de las rutas de acceso

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,26 @@
 const express = require('express');
-const app = express();
 const dotenv = require('dotenv');
+const { sequelizeFemale, sequelizeMale } = require('./config/database');
+const femalePlayerRoutes = require('./routes/femalePlayerRoutes');
+const malePlayerRoutes = require('./routes/malePlayerRoutes');
+const app = express();
 
 dotenv.config();
-
 app.use(express.json());
+
+app.use('/api/players/male', malePlayerRoutes);
+app.use('/api/players/female', femalePlayerRoutes);
+
+sequelizeMale.authenticate()
+  .then(() => console.log('## Connected to the database of male players ##'))
+  .catch(error => console.error('Connection error for male players:', error));
+
+sequelizeFemale.authenticate()
+   .then(() => console.log('## Connected to the database of female players ##'))
+   .catch(error => console.error('Connection error for female players:', error));
+
+sequelizeMale.sync();
+sequelizeFemale.sync();
 
 app.listen(process.env.PORT, () => {
     console.log(`## SERVER RUNNING ON PORT ${process.env.PORT} ##`);
