3bfb2b211c19008fdeb20732b1ea2ba8e8cb5404
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-11-03 14:56:19 -0300

    Configuracion de variables de entorno

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,4 +1,6 @@
 const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const sequelizeFemale = new Sequelize(process.env.DB_FEMALE_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
