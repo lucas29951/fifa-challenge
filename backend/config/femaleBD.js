5e95ceb32d1a0630a4f31af842bde3f36ebd5df6
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-11-03 14:13:32 -0300

    Redefinicion de la configuracion de base de datos

---
+++ backend/config/femaleBD.js
@@ -0,0 +1,8 @@
+const { Sequelize } = require('sequelize');
+
+const sequelizeFemale = new Sequelize(process.env.DB_FEMALE_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+});
+
+module.exports = sequelizeFemale;
