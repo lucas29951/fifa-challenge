c32a49edee8bc9a47daeb20a4230ca5dbd725230
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-11-04 12:53:54 -0300

    Nuevas rutas definidas

--- backend/routes/femalePlayerRoutes.js
+++ backend/routes/femalePlayerRoutes.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router();
 const femalePlayerController = require('../controllers/femalePlayerController');
 
-router.get('/', femalePlayerController.getFemalePlayers);
 router.post('/', femalePlayerController.createFemalePlayer);
+router.get('/', femalePlayerController.getFemalePlayers);
+router.get('/id/:id', femalePlayerController.getPlayerByID);
+router.get('/name/:name', femalePlayerController.getPlayerByName);
+router.get('/club/:club', femalePlayerController.getPlayersByClub);
+router.get('/country/:country', femalePlayerController.getPlayersByCountry);
+router.get('/pagination', femalePlayerController.getPlayersPaginated);
+router.get('/last-version', femalePlayerController.getPlayersByLastVersion);
 
 module.exports = router;
--- backend/routes/malePlayerRoutes.js
+++ backend/routes/malePlayerRoutes.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router();
 const malePlayerController = require('../controllers/malePlayerController');
 
-router.get('/', malePlayerController.getMalePlayers);
 router.post('/', malePlayerController.createMalePlayer);
+router.get('/', malePlayerController.getMalePlayers);
+router.get('/id/:id', malePlayerController.getPlayerByID);
+router.get('/name/:name', malePlayerController.getPlayerByName);
+router.get('/club/:club', malePlayerController.getPlayersByClub);
+router.get('/country/:country', malePlayerController.getPlayersByCountry);
+router.get('/pagination', malePlayerController.getPlayersPaginated);
+router.get('/last-version', malePlayerController.getPlayersByLastVersion);
 
 module.exports = router;
