0d8d7c196bf341a748933c15892f7ce2b287fbc0
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-11-05 15:05:21 -0300

    Rutas base definidas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { PlayersListComponent } from './components/players-list/players-list.component';
+import { PlayerDetailComponent } from './components/player-detail/player-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'players/:genero', component: PlayersListComponent },
+    { path: 'player/:genero/:id', component: PlayerDetailComponent }
+];
