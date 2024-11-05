999abb5c9834088fd95e29e52f3d36b50398b326
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-11-05 15:04:30 -0300

    Configuracion de conexion a la base de datos modificada

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,13 +3,17 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const sequelizeFemale = new Sequelize(process.env.DB_FEMALE_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql'
+    host: process.env.DB_HOST_F,
+    dialect: 'mysql',
+    port: process.env.DB_PORT_F,
+    logging: false
 });
 
 const sequelizeMale = new Sequelize(process.env.DB_MALE_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql'
+    host: process.env.DB_HOST_M,
+    dialect: 'mysql',
+    port: process.env.DB_PORT_M,
+    logging: false
 });
 
 module.exports = { sequelizeFemale, sequelizeMale };
