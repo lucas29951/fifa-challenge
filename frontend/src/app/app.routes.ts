83af4f28fc29db3bf2b61471e33ec8a356ea04b2
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-11-10 16:52:16 -0300

    Se agrego una ruta para la vista de detalle

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,5 +6,5 @@ import { PlayerDetailComponent } from './components/player-detail/player-detail.
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'players', component: PlayersListComponent },
-    // { path: 'player/:genero/:id', component: PlayerDetailComponent }
+    { path: 'player/:genre/:id', component: PlayerDetailComponent }
 ];
