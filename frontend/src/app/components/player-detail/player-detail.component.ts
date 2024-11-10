018c5fb77beca7700fda7630eb1bfa2e82ee523b
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-11-10 16:56:12 -0300

    Logica del componente implementado

--- frontend/src/app/components/player-detail/player-detail.component.ts
+++ frontend/src/app/components/player-detail/player-detail.component.ts
@@ -2,26 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { PlayerService } from '../../services/player.service';
 import { Player } from '../../models/Player.model';
 import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-player-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './player-detail.component.html',
   styleUrl: './player-detail.component.css'
 })
 export class PlayerDetailComponent implements OnInit {
-  jugador!: Player;
-  // genero: 'hombres' | 'mujeres';
-  genero: string = '';
+  player: Player | null = null;
 
   constructor(private playerService: PlayerService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    const id =+! this.route.snapshot.paramMap.get('id');
-    this.genero = this.route.snapshot.paramMap.get('genero') as 'hombres' | 'mujeres';
-    this.playerService.getJugadorById(this.genero, id).subscribe(jugador => {
-      this.jugador = jugador;
+    const id = +this.route.snapshot.paramMap.get('id')!;
+    const genre = this.route.snapshot.paramMap.get('genre') as 'male' | 'female';
+
+    this.playerService.getJugadorById(genre, id).subscribe(data => {
+      this.player = data;
     });
   }
 }
