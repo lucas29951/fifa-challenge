84b8588e7bc9bf30a09505df8e1ad0917f23ad2c
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-08-08 00:30:23 -0300

    Se definio la ruta correspondiente para la edicion de jugadores

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { PlayersListComponent } from './components/players-list/players-list.component';
 import { PlayerDetailComponent } from './components/player-detail/player-detail.component';
+import { PlayerEditComponent } from './components/player-edit/player-edit.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'players', component: PlayersListComponent },
-    { path: 'player/:genre/:id', component: PlayerDetailComponent }
+    { path: 'player/:genre/:id', component: PlayerDetailComponent },
+    { path: 'player/:genre/:id/edit', component: PlayerEditComponent }
 ];
