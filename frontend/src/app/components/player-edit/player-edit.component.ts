029a1d1b777d45c718753d05d106d9539ab493fc
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-08-08 00:29:04 -0300

    Logica de la vista de edicion implementada

--- frontend/src/app/components/player-edit/player-edit.component.ts
+++ frontend/src/app/components/player-edit/player-edit.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PlayerService } from '../../services/player.service';
+import { Player } from '../../models/Player.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-player-edit',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './player-edit.component.html',
   styleUrl: './player-edit.component.css'
 })
-export class PlayerEditComponent {
+export class PlayerEditComponent implements OnInit {
+  player: any = {};
+  genero!: string;
+  id!: number;
 
+  constructor(
+    private route: ActivatedRoute,
+    private playerService : PlayerService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+      this.genero = this.route.snapshot.paramMap.get('genre') as 'male' | 'female';
+      this.id = +this.route.snapshot.paramMap.get('id')!;
+
+      this.playerService.getJugadorById(this.genero, this.id).subscribe(data => {
+        this.player = data;
+      });
+  }
+
+  onSubmit(): void {
+    this.playerService.updateJugador(this.genero, this.id, this.player).subscribe(() => {
+      alert('Jugador actualizado correctamente!');
+      this.router.navigate(['/players']);
+    });
+  }
 }
