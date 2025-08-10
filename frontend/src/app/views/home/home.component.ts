d23cf2c1abe952b1beb49d7910880178df6b07b5
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-08-10 00:34:08 -0300

    Logica de la vista implementada para la carga de datos

--- frontend/src/app/views/home/home.component.ts
+++ frontend/src/app/views/home/home.component.ts
@@ -1,15 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Player } from '../../models/Player.model';
+import { CommonModule } from '@angular/common';
+import { PlayerService } from '../../services/player.service';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  featuredPlayers: Player[] = [];
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private playerService: PlayerService
+  ) {}
 
+  ngOnInit(): void {
+      forkJoin({
+      males: this.playerService.getMalePlayers(1,3),
+      females: this.playerService.getFemalePlayers(1,3)
+    }).subscribe(({ males, females }) => {
+      this.featuredPlayers = [
+        ...males.map(player => ({ ...player, genre: 'male' })),
+        ...females.map(player => ({ ...player, genre: 'female' }))
+      ];
+      this.featuredPlayers.sort((a,b) => a.long_name.localeCompare(b.long_name));
+    });
+  }
 }
