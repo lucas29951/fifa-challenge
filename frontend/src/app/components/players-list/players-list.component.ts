eb6b6314251fde8bc9c6f0866ebc033bd93f4d6d
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-08-09 21:17:54 -0300

    Logica de paginacion añadida a la vista

--- frontend/src/app/components/players-list/players-list.component.ts
+++ frontend/src/app/components/players-list/players-list.component.ts
@@ -15,8 +15,14 @@ import { RouterLink } from '@angular/router';
 })
 export class PlayersListComponent implements OnInit {
   @Input() players: Player[] = [];
+  currentPage = 1;
+  totalPages = 1;
+  limit = 10;
 
-  constructor(private playerService: PlayerService, private route: ActivatedRoute) {}
+  constructor(
+    private playerService: PlayerService, 
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.cargarJugadores();
@@ -24,14 +30,22 @@ export class PlayersListComponent implements OnInit {
 
   cargarJugadores() {
     forkJoin({
-      males: this.playerService.getMalePlayers(),
-      females: this.playerService.getFemalePlayers()
-    }).subscribe(({ males, females}) => {
+      males: this.playerService.getMalePlayers(this.currentPage, this.limit),
+      females: this.playerService.getFemalePlayers(this.currentPage, this.limit)
+    }).subscribe(({ males, females }) => {
       this.players = [
         ...males.map(player => ({ ...player, genre: 'male' })),
         ...females.map(player => ({ ...player, genre: 'female' }))
       ];
       this.players.sort((a,b) => a.long_name.localeCompare(b.long_name));
+      this.totalPages = Math.ceil(this.players.length / this.limit);
     });
+  }
+
+  changePage(page: number): void {
+    if (page >= 1 && page <= this.totalPages) {
+      this.currentPage = page;
+      this.cargarJugadores();
+    }
   }
 }
