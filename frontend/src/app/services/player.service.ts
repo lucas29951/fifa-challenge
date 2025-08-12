88a27e7466a401dd72e2091efe93ae59066a5130
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-08-12 16:24:23 -0300

    Add: metodo agregado al service para crear jugadores

--- frontend/src/app/services/player.service.ts
+++ frontend/src/app/services/player.service.ts
@@ -62,4 +62,8 @@ export class PlayerService {
 
     return this.http.get<Player[]>(`${this.apiUrl}/${filters.gender}/filter`, { params });
   }
+
+  createPlayer(data: any) {
+    return this.http.post<any>(this.apiUrl, data);
+  }
 }
