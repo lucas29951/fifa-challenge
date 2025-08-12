d9149aa5eed9e896b9d2a7bc736376e4e44307d7
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-08-12 02:07:57 -0300

    Login de cambio de estado - logged, logout

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  isLoggedIn = false;
+  userName: string | null = null;
 
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+      const token = this.authService.getToken();
+
+      if (token) {
+        this.isLoggedIn = true;
+
+        const storedName = localStorage.getItem('userName');
+        if (storedName) {
+          this.userName = storedName;
+        }
+      }
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.isLoggedIn = false;
+    this.userName = null;
+    this.router.navigate(['/login']);
+  }
 }
